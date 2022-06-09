8bce0d15817f7b9f42d237e6e1794ed40449d2c8
michael beloff <michael.a.beloff@gmail.com>
2022-06-09 13:43:50 +1000

    show cost preview for extra fee changes

--- src/components/ModifyFees.vue
+++ src/components/ModifyFees.vue
@@ -3,9 +3,107 @@
     <loading-overlay v-if="loading"></loading-overlay>
     <p class="my-3 text-sm text-gray-500" v-if="store.mode == 2">
       Add extra options on your booking. Please note you may not be able to
-      downgrade or remove certain options you have previously selected. Please
+      downgrade or remove certain options via this check-in platform. Please
       contact us if you wish to change these options.
     </p>
+    <div v-if="availablefees.insuranceoptions.length" class="text-sm">
+      <my-header :title="'Damage Cover'"></my-header>
+      <p class="mb-1 text-gray-500">
+        Mandatory for all hires. Limits your liability in the case of an
+        incident.
+      </p>
+      <div
+        v-for="item in availablefees.insuranceoptions"
+        class="mb-1 rounded border border-red-600 bg-gray-200 px-2 py-2"
+        :class="{
+          'border-transparent opacity-80': selecteddamage != item.id,
+        }"
+      >
+        <label :for="'dc' + item.id" class="divide-y divide-gray-300">
+          <div class="flex justify-between">
+            <input
+              hidden
+              type="radio"
+              :id="'dc' + item.id"
+              :name="'dc' + item.id"
+              v-model="selecteddamage"
+              :value="item.id"
+            />
+            <div>{{ item.name }}</div>
+            <div
+              class="flex min-w-[70px] items-center font-bold"
+              :class="{ 'text-gray-400': selecteddamage != item.id }"
+            >
+              <i class="fal fa-plus-circle mr-2"></i
+              >{{ currencysymbol + item.fees
+              }}<span class="text-xs font-normal">/day</span>
+            </div>
+          </div>
+
+          <div
+            v-html="
+              item.feedescription1
+                .replaceAll('yes', check)
+                .replaceAll('xy', '</br>')
+                .replaceAll('yt', '')
+            "
+            class="pt-2 text-xs text-gray-600"
+          ></div>
+        </label>
+      </div>
+    </div>
+    <div v-if="availablefees.kmcharges.length" class="text-sm">
+      <my-header :title="'Daily Kilometre Options'"></my-header>
+      <div
+        v-for="item in availablefees.kmcharges"
+        :key="item.id"
+        class="mb-1 rounded border border-red-600 bg-gray-200 px-2 py-2"
+        :class="{
+          'border-transparent opacity-80': selectedkm != item.id,
+        }"
+      >
+        <label
+          v-if="
+            !isDowngrade(
+              item.totalamount,
+              store.bookinginfo.bookinginfo[0].kmcharges_totalfordailyrate
+            )
+          "
+          :for="'km' + item.id"
+          class="divide-y divide-gray-300"
+        >
+          <div class="flex justify-between">
+            <input
+              hidden
+              :disabled="
+                isDowngrade(
+                  item.totalamount,
+                  store.bookinginfo.bookinginfo[0].kmcharges_totalfordailyrate
+                )
+              "
+              type="radio"
+              :id="'km' + item.id"
+              :name="'km' + item.id"
+              v-model="selectedkm"
+              :value="item.id"
+            />
+            <div>{{ item.description }}</div>
+            <div>
+              <div
+                class="flex min-w-[70px] items-center font-bold"
+                :class="{ 'text-gray-400': selectedkm != item.id }"
+              >
+                <i class="fal fa-plus-circle mr-2"></i
+                >{{ currencysymbol + item.dailyrate
+                }}<span class="text-xs font-normal">/day</span>
+              </div>
+            </div>
+          </div>
+
+          <div v-if="item.notes" v-html="item.notes" class="pt-2 text-xs"></div>
+        </label>
+      </div>
+    </div>
     <div v-if="availablefees" class="text-sm">
       <div class="flex flex-col gap-1" v-if="availablefees.optionalfees.length">
         <my-header :title="'Optional Fees'"></my-header>
@@ -14,12 +112,13 @@
           :key="item.id"
           class="rounded border border-red-600 bg-gray-200 px-2 py-2"
           :class="{
-            'border-transparent bg-transparent opacity-80':
-              !selectedoptions.find((el) => el.id == item.id),
+            'border-transparent opacity-80': !selectedoptions.find(
+              (el) => el.id == item.id
+            ),
           }"
         >
-          <label :for="'option' + item.id">
-            <div class="mb-2 flex justify-between border-b border-gray-900/20">
+          <label :for="'option' + item.id" class="divide-y divide-gray-300">
+            <div class="flex justify-between">
               <input
                 hidden
                 :disabled="isDisabled(item.id)"
@@ -31,7 +130,7 @@
               />
               <div>{{ item.name }}</div>
               <div
-                class="flex min-w-[70px] items-center font-bold"
+                class="flex items-center font-bold"
                 :class="{
                   'text-gray-400': !selectedoptions.find(
                     (el) => el.id == item.id
@@ -39,122 +138,33 @@
                 }"
               >
                 <i class="fal fa-plus-circle mr-2"></i
-                >{{ currencysymbol + item.totalfeeamount }}
-              </div>
-            </div>
-            <div v-html="item.feedescription1"></div>
-          </label>
-        </div>
-      </div>
-      <div v-if="availablefees.insuranceoptions.length">
-        <my-header :title="'Damage Cover'"></my-header>
-        <div
-          v-for="item in availablefees.insuranceoptions"
-          class="rounded border border-red-600 bg-gray-200 px-2 py-2"
-          :class="{
-            'border-transparent bg-transparent opacity-80':
-              selecteddamage != item.id,
-          }"
-        >
-          <label :for="'km' + item.id">
-            <div class="mb-2 flex justify-between border-b border-gray-900/20">
-              <input
-                hidden
-                type="radio"
-                :id="'km' + item.id"
-                :name="'km' + item.id"
-                v-model="selecteddamage"
-                :value="item.id"
-              />
-              <div>{{ item.name }}</div>
-              <div
-                class="flex min-w-[70px] items-center font-bold"
-                :class="{ 'text-gray-400': selecteddamage != item.id }"
-              >
-                <i class="fal fa-plus-circle mr-2"></i
-                >{{ currencysymbol + item.totalinsuranceamount }}
-              </div>
-            </div>
-            <p class="mb-1 text-gray-600">
-              Mandatory for all hires. Limits your liability in the case of an
-              incidicent.
-            </p>
-            <div
-              v-html="
-                item.feedescription1
-                  .replaceAll('yes', check)
-                  .replaceAll('xy', '</br>')
-                  .replaceAll('yt', '')
-              "
-              class="text-gray-600"
-            ></div>
-          </label>
-        </div>
-      </div>
-      <div v-if="availablefees.kmcharges.length">
-        <my-header :title="'Daily Kilometre Options'"></my-header>
-        <div
-          v-for="item in availablefees.kmcharges"
-          :key="item.id"
-          class="rounded border border-red-600 bg-gray-200 px-2 py-2"
-          :class="{
-            'border-transparent bg-transparent opacity-80':
-              selectedkm != item.id,
-          }"
-        >
-          <label
-            v-if="
-              !isDowngrade(
-                item.totalamount,
-                store.bookinginfo.bookinginfo[0].kmcharges_totalfordailyrate
-              )
-            "
-            :for="'km' + item.id"
-          >
-            <div class="flex justify-between">
-              <input
-                hidden
-                :disabled="
-                  isDowngrade(
-                    item.totalamount,
-                    store.bookinginfo.bookinginfo[0].kmcharges_totalfordailyrate
-                  )
-                "
-                type="radio"
-                :id="'km' + item.id"
-                :name="'km' + item.id"
-                v-model="selectedkm"
-                :value="item.id"
-              />
-              <div>{{ item.description }}</div>
-              <div>
-                <div
-                  class="flex min-w-[70px] items-center font-bold"
-                  :class="{ 'text-gray-400': selectedkm != item.id }"
+                >{{ currencysymbol + item.fees
+                }}<span v-if="item.type == 'Daily'" class="text-xs font-normal"
+                  >/day</span
                 >
-                  <i class="fal fa-plus-circle mr-2"></i
-                  >{{ currencysymbol + item.totalamount }}
-                </div>
               </div>
             </div>
-
-            <div
-              v-if="item.notes"
-              v-html="item.notes"
-              class="border-t border-gray-900/20 pt-2 text-gray-600"
-            ></div>
+            <div v-html="item.feedescription1" class="pt-2 text-xs"></div>
           </label>
         </div>
       </div>
     </div>
-    <div class="flex w-full justify-end">
-      <my-button
-        v-show="changes.length"
-        class="btn-green mt-5 w-full sm:w-1/2"
-        @click="editbooking()"
-      >
-        Save Changes <i class="far fa-cloud-upload"></i>
-      </my-button>
+    <div class="flex w-full justify-end"></div>
+    <div class="relative mt-2 rounded bg-gray-200 p-2">
+      <loading-overlay v-if="calculating"></loading-overlay>
+      <div class="mx-auto w-full max-w-md">
+        <totals-preview
+          v-if="totals.length > 0"
+          :totals="totals"
+        ></totals-preview>
+        <my-button
+          v-show="changes.length"
+          class="btn-green mt-5 w-full"
+          @click="editbooking()"
+        >
+          Save Changes <i class="far fa-cloud-upload"></i>
+        </my-button>
+      </div>
     </div>
   </div>
 </template>
@@ -162,12 +172,14 @@
 <script setup>
 import MyButton from "@/components/base/MyButton.vue";
 import MyHeader from "@/components/base/MyHeader.vue";
+import TotalsPreview from "@/components/TotalsPreview.vue";
 import LoadingOverlay from "@/components/LoadingOverlay.vue";
 import { useStore } from "@/store";
-import { ref, inject, computed } from "vue";
-
+import { ref, inject, computed, onMounted, watch } from "vue";
+const emit = defineEmits(["update"]);
 const store = useStore();
 const rcm = inject("rcm");
+const calculating = ref(false);
 const loading = ref(false);
 const check = "<i class='fal fa-check-circle'></i>";
 const currencysymbol = computed(
@@ -179,6 +191,23 @@ const availablefees = await rcm({
 }).then((data) => {
   return data.results;
 });
+const totals = ref("");
+const initialkm = computed(() => store.bookinginfo.bookinginfo[0].kmcharges_id);
+const selectedkm = ref(initialkm.value);
+const initialdamage = computed(
+  () => store.bookinginfo.extrafees.find((el) => el.isinsurancefee).extrafeeid
+);
+const selecteddamage = ref(initialdamage.value);
+const initialoptions = computed(() => {
+  let arr = [];
+  store.bookinginfo.extrafees.forEach((el) => {
+    if (el.isoptionalfee && !el.isinsuranceoption) {
+      arr.push({ id: el.extrafeeid, qty: el.qty });
+    }
+  });
+  return arr;
+});
+const selectedoptions = ref(initialoptions.value);
 
 function isDowngrade(price, booked) {
   if (store.mode == 2) return price < booked;
@@ -192,6 +221,10 @@ function isDisabled(id) {
   } else return false;
 }
 
+onMounted(() => {
+  calcTotal();
+});
+
 const changes = computed(() => {
   let diffs = [];
   if (initialkm.value != selectedkm.value) diffs.push("km");
@@ -203,6 +236,10 @@ const changes = computed(() => {
   return diffs;
 });
 
+watch(changes, () => {
+  calcTotal();
+});
+
 function getDifference(array1, array2) {
   return array1.filter((object1) => {
     return !array2.some((object2) => {
@@ -210,8 +247,6 @@ function getDifference(array1, array2) {
     });
   });
 }
-
-const emit = defineEmits(["update"]);
 
 function editbooking() {
   loading.value = true;
@@ -235,28 +270,38 @@ function editbooking() {
     }
   }
   rcm(params).then((data) => {
-    console.log(data);
     loading.value = false;
     emit("update");
   });
 }
 
-const initialkm = computed(() => store.bookinginfo.bookinginfo[0].kmcharges_id);
-const selectedkm = ref(initialkm.value);
-const initialdamage = computed(
-  () => store.bookinginfo.extrafees.find((el) => el.isinsurancefee).extrafeeid
-);
-const selecteddamage = ref(initialdamage.value);
-const initialoptions = computed(() => {
-  let arr = [];
+function calcTotal() {
+  calculating.value = true;
+  let mandatoryfees = [];
   store.bookinginfo.extrafees.forEach((el) => {
-    if (el.isoptionalfee && !el.isinsuranceoption) {
-      arr.push({ id: el.extrafeeid, qty: el.qty });
+    if (!el.isoptionalfee) {
+      mandatoryfees.push({ id: el.extrafeeid, qty: el.qty });
     }
   });
-  return arr;
-});
-const selectedoptions = ref(initialoptions.value);
+  let params = {
+    method: "calctotal",
+    pickuplocationid: store.bookinginfo.bookinginfo[0].pickuplocationid,
+    pickupdate: store.bookinginfo.bookinginfo[0].pickupdate,
+    vehiclecategoryid: store.bookinginfo.bookinginfo[0].vehiclecategoryid,
+    numberofdays: store.bookinginfo.bookinginfo[0].numberofdays,
+    totalrateafterdiscount: store.bookinginfo.rateinfo[0].ratesubtotal,
+    insuranceid: selecteddamage.value,
+    extrakmsid: selectedkm.value,
+    optionalfees: selectedoptions.value,
+    mandatoryfees: mandatoryfees,
+    reservationref: store.resref,
+    freedaysamount: 0,
+  };
+  rcm(params).then((res) => {
+    totals.value = res.results.totals;
+    calculating.value = false;
+  });
+}
 </script>
 
 <style lang="scss" scoped></style>
---
+++ src/components/TotalsPreview.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="text-xs">
+    <p class="text-xs font-bold">Selected Options</p>
+    <div
+      v-for="el in props.totals.filter((el) => el.type == 'optional')"
+      class="flex justify-between"
+    >
+      <span>{{ el.name }}</span
+      ><span>{{ symbol + el.total.toFixed(2) }}</span>
+    </div>
+    <p class="text-xs font-bold">Mandatory Fees</p>
+    <div
+      v-for="el in props.totals.filter((el) => el.type == 'mandatory')"
+      class="flex justify-between"
+    >
+      <span>{{ el.name }}</span
+      ><span>{{ symbol + el.total.toFixed(2) }}</span>
+    </div>
+    <div class="mt-2 flex justify-between">
+      <span
+        >Daily Rate @
+        {{
+          symbol +
+          store.bookinginfo.rateinfo[0].dailyrateafterdiscount.toFixed(2)
+        }}</span
+      ><span>{{
+        symbol +
+        props.totals.find((el) => el.type == "total rate").total.toFixed(2)
+      }}</span>
+    </div>
+    <div class="mt-2 flex justify-between text-sm">
+      <span>Total</span
+      ><span>{{
+        symbol +
+        props.totals.find((el) => el.type == "total").total.toFixed(2)
+      }}</span>
+    </div>
+    <p class="text-right">(includes GST of: {{
+        symbol +
+        props.totals.find((el) => el.type == "country tax").total.toFixed(2)
+      }})</p>
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { useStore } from "@/store";
+const store = useStore();
+const props = defineProps({
+  totals: {
+    type: Array,
+    default: null,
+  },
+});
+const currency = computed(() => store.bookinginfo.bookinginfo[0].currencyname);
+const symbol = computed(() => store.bookinginfo.bookinginfo[0].currencysymbol);
+</script>
+
+<style lang="scss" scoped></style>
