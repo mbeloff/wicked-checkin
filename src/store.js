25978baeaf674ac70bfe3f5ef2d239139d170e5c
michael beloff <michael.a.beloff@gmail.com>
2022-08-01 15:46:36 +1000

    init .ca info

--- src/store.js
+++ src/store.js
@@ -17,11 +17,11 @@ export const useStore = defineStore("store", {
       },
       company: {
         name: "Wicked Campers",
-        tag: "Discount Camper Rentals, Australia",
+        tag: "Discount Camper Rentals, North America",
         email: "online@wickedcampers.com",
         phone: "+612 4046 7811",
-        website: "https://www.wickedcampers.com.au",
-        abn: "ABN 35 640 465 643",
+        website: "https://www.wickedcampers.ca",
+        // abn: "ABN 35 640 465 643",
       },
     };
   },
