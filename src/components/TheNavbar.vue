f51d156939f6898793726c6a0c4e61919734beff
michael beloff <michael.a.beloff@gmail.com>
2022-08-09 15:00:39 +1000

    remove number

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -5,12 +5,12 @@
         <a href="/" class="">
           <img :src="logo" class="w-32" />
         </a>
-        <div class="flex h-full w-full flex-grow items-center">
-          <a
+        <div class="flex h-full w-full flex-grow items-center justify-end">
+          <!-- <a
             :href="`tel:${store.company.phone.replaceAll(' ', '')}`"
             class="ml-auto min-w-max font-bold text-gray-300 hover:text-primary-600"
             >{{ store.company.phone }}</a
-          >
+          > -->
           <a
             :href="store.company.website"
             class="shadow-inset ml-4 min-w-max max-w-max rounded bg-white bg-gradient-to-tr from-primary-500 to-primary-600 px-5 py-2 text-sm font-bold uppercase text-white shadow-lg hover:from-primary-600/60 hover:to-primary-700/60"
