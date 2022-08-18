599b800ae0b84a45f4e328443f7d5484aea2db6c
michael beloff <michael.a.beloff@gmail.com>
2022-08-19 08:17:10 +1000

    fix color

--- tailwind.config.js
+++ tailwind.config.js
@@ -41,7 +41,7 @@ module.exports = {
       current: "currentColor",
       black: colors.black,
       white: colors.white,
-      gray: colors.slate,
+      gray: colors.zinc,
       blue: colors.sky,
       accent: colors.sky,
       primary: customred,
