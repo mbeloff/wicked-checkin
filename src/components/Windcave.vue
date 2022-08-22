1c10601ec6d7b5be0a478f599616ecc44a38b62a
michael beloff <michael.a.beloff@gmail.com>
2022-08-23 07:24:21 +1000

    convert quote back on

--- src/components/Windcave.vue
+++ src/components/Windcave.vue
@@ -122,14 +122,13 @@ watch(paymentResponse, (val) => {
         val.DateExpiry._text.slice(0, 2) + "/" + val.DateExpiry._text.slice(2),
       transtype: "Purchase",
       payscenario: store.mode == 1 ? 2 : 3,
-      emailoption: 0,
     };
 
     rcm(params)
       .then((res) => {
-        // if (store.mode == 1) {
-        //   convertQuote();
-        // }
+        if (store.mode == 1) {
+          convertQuote();
+        }
         emit("update");
       })
       .catch((err) => console.log(err));
@@ -139,35 +138,32 @@ watch(paymentResponse, (val) => {
   }
 });
 
-// function convertQuote() {
-//   let opts = store.bookinginfo.extrafees
-//     .filter((el) => el.isoptionalfee && !el.isinsurancefee)
-//     .map((el) => {
-//       return { id: el.extrafeeid, qty: el.qty };
-//     });
-//   let params = {
-//     method: "convertquote",
-//     reservationref: store.resref,
-//     emailoption: 1,
-//     extrakmsid: store.bookinginfo.bookinginfo[0].kmcharges_id,
-//     insuranceid: store.bookinginfo.extrafees.find((el) => el.isinsurancefee)
-//       .extrafeeid,
-//     customer: { ...store.bookinginfo.customerinfo[0] },
-//     optionalfees: opts,
-//   };
-//   rcm(params).then((data) => {
-//     if (data.status == "ERR")
-//       alert(
-//         "We were unable to convert this quote to a booking request.\nPlease try again and contact us if the problem persists."
-//       );
-//     if (data.status == "OK") {
-//       alert(
-//         "Thank you for requesting a reservation!\nKeep an eye on your email for a booking confirmation."
-//       );
-//       emit("update");
-//     }
-//   });
-// }
+function convertQuote() {
+  let opts = store.bookinginfo.extrafees
+    .filter((el) => el.isoptionalfee && !el.isinsurancefee)
+    .map((el) => {
+      return { id: el.extrafeeid, qty: el.qty };
+    });
+  let params = {
+    method: "convertquote",
+    reservationref: store.resref,
+    emailoption: 1,
+    extrakmsid: store.bookinginfo.bookinginfo[0].kmcharges_id,
+    insuranceid: store.bookinginfo.extrafees.find((el) => el.isinsurancefee)
+      .extrafeeid,
+    customer: { ...store.bookinginfo.customerinfo[0] },
+    optionalfees: opts,
+  };
+  rcm(params).then((data) => {
+    if (data.status == "ERR") console.log("error converting quote");
+    if (data.status == "OK") {
+      alert(
+        "Thank you for requesting a reservation!\nKeep an eye on your email for a booking confirmation."
+      );
+      emit("update");
+    }
+  });
+}
 </script>
 
 <style lang="scss"></style>
