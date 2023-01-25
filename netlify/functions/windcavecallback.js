a63c2e498049cd16381a38e55060858fdf56af9d
michael beloff <michael.a.beloff@gmail.com>
2023-01-25 15:28:36 +1000

    fix tnxtype

--- netlify/functions/windcavecallback.js
+++ netlify/functions/windcavecallback.js
@@ -58,7 +58,7 @@ exports.handler = async function (event) {
     cardnumber: json.CardNumber._text,
     cardexpiry:
       json.DateExpiry._text.slice(0, 2) + "/" + json.DateExpiry._text.slice(2),
-    transtype: json.TxnType._text,
+    transtype: json.TxnType._text == 'Auth' ? 'Auth' : 'Payment',
   };
 
   let token = await fetch("https://checkin.wickedcampers.co.nz/.netlify/functions/getToken", {
