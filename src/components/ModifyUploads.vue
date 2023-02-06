cd23990cde610dd09c204ddd0d56f9a31ccad41b
michael beloff <michael.a.beloff@gmail.com>
2023-02-06 15:31:41 +1000

    -

--- src/components/ModifyUploads.vue
+++ src/components/ModifyUploads.vue
@@ -185,7 +185,7 @@ export default {
         return "Take a photo of the front of your driver's license";
       }
       if (text == "Take a Selfie") {
-        return "Take a photo of yourself";
+        return "Take a photo of yourself holding your license";
       }
       if (text == "Take a photo of the back of your driving license") {
         return "Take a photo of the back of your driver's license";
